3a84ea158a414b2d46de9238e6a2f7096d955c3d
Alexander <a.munirov@ddxfitness.ru>
2023-05-17 13:13:02 +0300

    first commit

---
+++ tests/first.test.ts
@@ -0,0 +1,7 @@
+import { test } from "@playwright/test";
+
+test.describe("Описание группы тестов", async () => {
+    test("Пример теста", async () => {
+        console.log("Уиии!! Я написал тест!");
+    });
+})
